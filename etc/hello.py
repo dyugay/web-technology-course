cba59e866180d4619548a0552faff8cb553b807c
dyugay <dyugay@yandex.ru>
2016-03-11 05:28:05 +0600

    commit

--- etc/hello.py
+++ etc/hello.py
@@ -1,5 +1,5 @@
 bind = "0.0.0.0:8080"
-workers = 4
-logfile = "/home/box/web/log/gunicorn.log"
-loglevel = "info"
-proc_name = "hello"
+workers = 3
+#logfile = "/home/box/web/log/gunicorn.log"
+#loglevel = "info"
+#proc_name = "hello"

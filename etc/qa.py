927340067bfe9f7b10a1e61f73fe40cd74619abc
dyugay <dyugay@yandex.ru>
2016-03-13 06:37:19 +0000

    commit qa.py

--- etc/qa.py
+++ etc/qa.py
@@ -2,11 +2,9 @@ CONFIG = {
     'mode': 'wsgi',
     'working_dir': '/home/box/web/ask/',
     'args': (
-         'wsgi',
+         'ask.wsgi',
          '--bind=0.0.0.0:8000',
-         '--workers=3',
-         '--access-logfile acc.log',
-         '--error-logfile err.log', 
+         '--workers=3', 
      ),     
  
 }

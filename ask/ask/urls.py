1905b49df11c1fac92a01e00b0bc5aa4931fb286
dyugay <dyugay@yandex.ru>
2016-03-13 06:24:54 +0000

    commit

---
+++ ask/ask/urls.py
@@ -0,0 +1,22 @@
+from django.conf.urls import patterns, include, url
+
+from django.contrib import admin
+admin.autodiscover()
+
+from qa.views import test
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'ask.views.home', name='home'),
+    # url(r'^blog/', include('blog.urls')),
+
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', 'qa.views.test1'),
+    url(r'^login/.*$', 'qa.views.test1'),
+    url(r'^signup/.*$', 'qa.views.test1'),
+    url(r'^question/', 'qa.views.test'),
+    url(r'^ask/.*$', 'qa.views.test1'),
+    url(r'^popular/.*$', 'qa.views.test1'),
+    url(r'^new/.*$', 'qa.views.test1') 
+)
+

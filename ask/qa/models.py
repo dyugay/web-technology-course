7b9a5a8fdf41143cfb2f128bbd8b60c133bed42b
dyugay <dyugay@yandex.ru>
2016-03-15 05:29:58 +0600

    after successful test

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -5,12 +5,12 @@ from django.contrib.auth.models import User
 # Create your models here.
 
 class Question(models.Model):
-  title = models.Charfield(max_length=255)
+  title = models.CharField(max_length=255)
   text = models.TextField()
   added_at = models.DateTimeField(auto_now_add=True)
   rating = models.IntegerField() 
   author = models.ForeignKey(User, null=True, on_delete=models.DO_NOTHING)
-  likes = models.ManyToManyField(User)
+  likes = models.ManyToManyField(User, related_name = 'likes_set')
   
 
 

da70b1be5da651ca747a1bf7cb32a977e7e6280f
dyugay <dyugay@yandex.ru>
2016-03-20 20:47:35 +0600

    all last changes

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import User
-
+from django.core.paginator import Paginator, EmptyPage
+from django.http import Http404
+from django.core.urlresolvers import reverse
 
 # Create your models here.
 
@@ -12,7 +14,9 @@ class Question(models.Model):
   author = models.ForeignKey(User, null=True, on_delete=models.DO_NOTHING)
   likes = models.ManyToManyField(User, related_name = 'likes_set')
   
-
+  def get_url(self):
+      return reverse('question-details',
+                     kwargs = {'questionId': self.id} ) 
 
 class Answer(models.Model):
   text = models.TextField()
@@ -21,5 +25,59 @@ class Answer(models.Model):
   author = models.ManyToManyField(User)
 
 
+#class qaShortcuts(object):   
+    
+ #    def __init__(self):
+  #           pass
+   
+def paginate(request, querySet):
+#shortcuts for pagination
+
+            #gets and checks limit value
+            try: 
+                  limit =  int(request.GET.get('limit', 10))
+            except ValueError:
+                  limit = 10      
+            if limit > 100:
+                  limit = 10
+           
+            
+            #gets and checks page value
+            try:
+                  page = int(request.GET.get('page', 1))
+            except ValueError:
+                  raise Http404
+            
+           
+            paginator = Paginator(querySet, limit)             
+            try:
+                  page = paginator.page(page)   
+            except EmptyPage:
+                  page = paginator.page(paginator.num_pages)
+            
+            paginatorAttr = {'page': page,
+                             'limit': limit,
+                             'paginator': paginator }
+            
+            return paginatorAttr
+      
+           
+            
+
  
 
+
+
+
+             
+
+
+
+
+
+
+
+    
+
+
+ 
--- ask/qa/views.py
+++ ask/qa/views.py
@@ -1,8 +1,63 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, get_object_or_404
+from qa.models import Question, Answer, paginate
+from django.contrib.auth.models import User
+from django.core.paginator import Paginator
 from django.http import HttpResponse
+from django.core.urlresolvers import reverse
 
-# Create your views here.
+
 def test(request, *args, **kwargs):
     return HttpResponse('OK')
+
+#get questions list at home page
+def getNewQuestionsList(request, *args, **kwargs):
+    questionsList = Question.objects.all()
+    questionsList = questionsList.order_by('-id')
+    paginatorAttr = paginate(request, questionsList) 
+   # shortcut = qaShortcuts()
+   # page = paginate(request, questionsList)['page']
+   # numPage = page = request.GET.get('page', 1)
+   # limit = request.GET.get('limit', 10)
+   # paginator = Paginator(questionsList, limit)
+   # page = paginator.page(page)
+    url = '/?page='  
+   # questionUrl = reverse('question-details')  
+
+    return render(request, 'qa/questionsList.html', {
+             # 'questionsList': paginatorAttr['page'].object_list,
+              'paginator': paginatorAttr['paginator'],
+              'page': paginatorAttr['page'], 
+              'url': url,                
+           })
+           
+
+def getPopularQuestions(request, *args, **kwargs):
+    questionsList = Question.objects.all()
+    questionsList = questionsList.order_by('rating')
+    paginatorAttr = paginate(request, questionsList)
+    url = '/popular/?page='
+    return render(request, 'qa/questionsList.html', {
+             # 'questionsList': paginatorAttr['page'].object_list,
+              'paginator': paginatorAttr['paginator'],
+              'page': paginatorAttr['page'],
+              'url': url,
+           })
+
+
+ #   return HttpResponse('popular')
+
+
+def questionDetails(request, *args, **kwargs):
+    questionId = kwargs['questionId']
+    question = get_object_or_404(Question, id = questionId)
+    answers = Answer.objects.filter(question = question)
+ 
+    # return HttpResponse(questionId)
+    return render(request, 'qa/questionDetails.html', {
+                      'question': question,                       
+                      'answers': answers, 
+                       })
+
+
+ 
 

0239feee8bad378bd4bacea1c89a51d281bbd681
dyugay <dyugay@yandex.ru>
2016-03-21 09:44:01 +0600

    Update init.sh

--- init.sh
+++ init.sh
@@ -3,4 +3,6 @@ sudo ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test
 sudo /etc/init.d/nginx restart
 sudo ln -s /home/box/web/etc/hello.py /etc/gunicorn.d/hello
 sudo ln -s /home/box/web/etc/qa.py /etc/gunicorn.d/qa
+sudo /etc/initd.d/gunicorn start
+sudo service mysql start
 
